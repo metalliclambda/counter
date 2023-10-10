c532bc1b33d153f6f5c5b05ee2a3fb6f937ff520
Mehdi M <66296756+metalliclambda@users.noreply.github.com>
2023-10-10 11:21:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -39,4 +39,85 @@ document.addEventListener("DOMContentLoaded", function() {
       total.querySelector(".number").textContent = sum;
     }
   });
+
+
+
+  // add to make pdf
+  // function generatePDF() {
+  //   console.log("MM");
+  //   var content = document.documentElement.outerHTML;
+  //   // Code to generate PDF using a PDF library or API
+  //   // You can use libraries like jsPDF, pdfmake, or use a PDF generation API like PDFShift, etc.
+  //   // Here's an example using jsPDF:
+  //   const { jsPDF } = window.jspdf
+  //   var doc = new jsPDF();
+  //   doc.text(content, 10, 10);
+  //   doc.save("page_content.pdf");
+  // }
+  // document.getElementById("generatePDFButton").addEventListener("click", generatePDF);  
+
+  // function exportToExcel() {
+  //   console.log("MM");
+  //   // Get the data from the HTML elements
+  //   const items = Array.from(document.querySelectorAll('.item'));
+  //   const data = items.map(item => ({
+  //     name: item.querySelector('.name').textContent,
+  //     number: item.querySelector('.number').textContent
+  //   }));
+  
+  //   // Create a new workbook
+  //   const workbook = XLSX.utils.book_new();
+  
+  //   // Create a new worksheet
+  //   const worksheet = XLSX.utils.json_to_sheet(data);
+  
+  //   // Add the worksheet to the workbook
+  //   XLSX.utils.book_append_sheet(workbook, worksheet, 'Items');
+  
+  //   // Generate the Excel file
+  //   const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+  
+  //   // Save the file using FileSaver.js (you need to include the library)
+  //   saveAs(new Blob([excelBuffer], { type: 'application/octet-stream' }), 'items.xlsx');
+  // }
+
+  function exportToExcel() {
+    // Get the data from the HTML elements
+    const items = Array.from(document.querySelectorAll('.item'));
+    const totalItem = document.getElementById('total');
+    const totalNumber = totalItem.querySelector('.number').textContent;
+    
+    const data = items.map(item => ({
+      name: item.querySelector('.name').textContent,
+      number: item.querySelector('.number').textContent
+    }));
+    data.push({
+      name : "Total",
+      number : totalNumber
+    });
+    
+    // Create a new worksheet
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    
+  
+    // Create a new workbook
+    const workbook = XLSX.utils.book_new();
+    
+    // Add the worksheet to the workbook
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Items');
+
+    // Generate the Excel file
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+  
+    // Save the file using FileSaver.js (you need to include the library)
+    saveAs(new Blob([excelBuffer], { type: 'application/octet-stream' }), 'items.xlsx');
+  }
+
+
+  
+  
+  document.getElementById("exceloutput").addEventListener("click", exportToExcel);  
+
+  
+
 });
